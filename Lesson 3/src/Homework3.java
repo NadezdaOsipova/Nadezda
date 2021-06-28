68dcd7d39f5b4077154ee387527565feb6156ba5
NadezdaOsipova <kaliadina@mail.ru>
2021-06-28 07:33:35 +0300

    Домашнее задание №3

---
+++ Lesson 3/src/Homework3.java
@@ -0,0 +1,169 @@
+import java.util.Scanner;
+
+public class Homework3 {
+
+    public static void main(String[] args) {
+        //Некоторые тесты для проверки задач. Можно также написать свои тесты.
+        printArray();
+        System.out.println(operation(1));
+        System.out.println(operation(0));
+        System.out.println(calculateCountOfOddElementsInMatrix(new int[]{1, 2, 3, 4, 5, 6}));
+        calculateSumOfDiagonalElements();
+        countDevs(103);
+        foobar(6);
+        foobar(10);
+        foobar(15);
+        printMatrix();
+        printPrimeNumbers();
+    }
+
+    /**
+     * Необходимо прочитать с консоли значение числа типа int,
+     * далее создать одноменрый массив типа int размера прочитаного с консоли
+     * далее заполнить массив случайными значениями
+     * далее вывести массив на консоль
+     */
+
+    private static void printArray() {
+        int num;
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Введите число: ");
+            num = in.nextInt();
+        }
+
+        int[] printArray = new int[num];
+        for (int i = 0; i < printArray.length; i++) {
+            printArray[i] = (int) (Math.random() * num);
+        }
+        for (int i : printArray) {
+            System.out.println(i + " ");
+
+        }
+        // тут пишем логику
+    }
+
+    /**
+     * Метод должен выполнять некоторую операцию с int "number" в зависимости от некоторых условий:
+     * - if number положительное число, то необходимо number увеличить на 1
+     * - if number отрицательное - уменьшить на 2
+     * - if number равно 0 , то замените значение number на 10
+     * вернуть number после выполнения операций
+     */
+    public static int operation(int number) {
+        if (number > 0) {
+            number = number + 1;
+        } else if (number < 0) {
+            number = number - 2;
+        } else number = 10;
+        return number;
+    }
+
+
+    /**
+     * На вход приходит массив целых чисел типа int
+     * Необходимо найти количество нечетных элементов в массиве и вернуть значение в метод main,
+     * в котором это значение распечатается на консоль.
+     */
+
+    public static int calculateCountOfOddElementsInMatrix(int[] ints) {
+        int[] Array = new int[]{1, 2, 3, 4, 5, 6};
+        int k = 0;
+        for (int m = 0; m < Array.length; m++) {
+            if (Array[m] % 2 != 0) {
+                k++;
+            }
+        }
+        return k;
+    }
+
+    /**
+     * На вход приходит число.
+     * Вывести в консоль фразу из разряда "_COUNT_ программистов",
+     * заменить _COUNT_ на число которое пришло на вход в метод и заменить окончание в слове "программистов" на
+     * уместное с точки зрения русского языка.
+     * Пример: 1 программист, 42 программиста, 50 программистов
+     *
+     * @param count - количество программистов
+     */
+    public static void countDevs(int count) {
+        int d;
+        Scanner in = new Scanner(System.in))
+        System.out.println("Введите число программистов ");
+        d = in.nextInt();
+
+
+        // тут пишем логику
+    }
+
+    /**
+     * Метод должен выводить разные строки в консоли в зависимости от некоторых условий:
+     * - если остаток от деления на 3 равен нулю - выведите "foo" (example of number - 6)
+     * - если остаток от деления на 5 равен нулю - вывести "bar" (example of number - 10)
+     * - если остаток от деления на 3 и 5 равен нулю 0 ,то вывести "foobar" (example of number - 15)
+     */
+    public static void foobar(int number) {
+        // тут пишем логику
+    }
+
+    /**
+     * заполнить рандомно 2-х мерный массив и посчитать сумму элементов на диагонали
+     */
+    public static void calculateSumOfDiagonalElements() {
+        //пишем логику и выводим результат используя System.out.println
+    }
+
+
+    /**
+     * Шаги по реализации:
+     * - Прочитать два int из консоли
+     * - Создайте двумерный массив int (используйте целые числа, которые вы читаете по высоте и ширине консоли)
+     * - Заполнить массив случайными значениями (до 100)
+     * - Вывести в консоль матрицу заданного размера, но:
+     * - Если остаток от деления элемента массива на 3 равен нулю - выведите знак "+" вместо значения элемента массива.
+     * - Если остаток от деления элемента массива на 7 равен нулю - выведите знак "-" вместо значения элемента массива.
+     * - В противном случае выведите "*"
+     * <p>
+     * Example:
+     * - Значения с консоли - 2 и 3
+     * - Массив будет выглядеть так (значения будут разными, потому что он случайный)
+     * 6 11 123
+     * 1 14 21
+     * - Для этого значения вывод в консоли должен быть:
+     * <p>
+     * + * *
+     * * - +
+     * <p>
+     * Обратите внимание, что 21% 3 == 0 и 21% 7 = 0, но выводить надо не +-, а +
+     */
+    public static void printMatrix() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int firstNumber = in.nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = in.nextInt();
+
+        int[][] myArray = new int[firstNumber][secondNumber];
+        for (int i = 0; i < myArray.length; i++) {
+            for (int j = 0; j < myArray[i].length; j++) {
+                myArray[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        for (int i : myArray) {
+            System.out.println(i + " ");
+        }
+
+        // дорешать задачу
+
+    }
+
+    /**
+     * Задача со звездочкой!
+     * Метод должен печатать все простые числа <1000
+     * что такое просто число (https://www.webmath.ru/poleznoe/formules_18_5.php)
+     */
+    public static void printPrimeNumbers() {
+        // тут пишем логику
+    }
+}
+
+

73b3c16515b51ede15611290a72c93d1004ce69b
NadezdaOsipova <kaliadina@mail.ru>
2021-07-18 00:39:36 +0300

    Homework3

--- Lesson 3/src/Homework3.java
+++ Lesson 3/src/Homework3.java
@@ -198,7 +198,19 @@ public class Homework3 {
      * Метод должен печатать все простые числа <1000
      * что такое просто число (https://www.webmath.ru/poleznoe/formules_18_5.php)
      */
+    public static void printPrimeNumbers() {
+        int i, j;
+        for (i = 2; i < 1000; i++) {
+            int k = 0;
 
+            for (j = 2; j <= i; j++) {
+                if ((i % j) == 0)
+                    k++;
+            }
+            if (k < 2)
+                System.out.print(i + " ");
+        }
+    }
 }
 
 

d5ad71872ba50f27cc8fe4158f0d791d1cac51ba
NadezdaOsipova <kaliadina@mail.ru>
2021-07-24 13:00:10 +0300

    Homework6

--- Lesson6/src/main/java/com/home/model/PersonRegistry.java
+++ Lesson6/src/main/java/com/home/model/PersonRegistry.java
@@ -5,18 +5,18 @@ public class PersonRegistry {
     private static final int MAX_AGE_PERSON = 27;
     private static final String SEX_PERSON = "MALE";
 
-    private Person[] persons; //массив случайных людей
+    private final Person[] persons; //массив случайных людей
 
-    public PersonRegistry(Person[] people) {
-        this.persons = people;
+    public PersonRegistry(Person[] persons) {
+        this.persons = persons;
     }
 
     public int countPeople(Address address) { // проживающие в городе Минск
         int count = 0;
         for (int i = 0; i < persons.length; i++) {
             Address personsAddress = persons[i].getAddress();
-            if (personsAddress.getCountry().equals(address.getCountry()) &&
-                    personsAddress.getCity().equals(address.getCity())) {
+            if (personsAddress.getCountry().equalsIgnoreCase(address.getCountry()) &&
+                    personsAddress.getCity().equalsIgnoreCase(address.getCity())) {
                 count++;
             }
         }
@@ -26,7 +26,7 @@ public class PersonRegistry {
     public int countPeopleAlex(String name) { //  с именем Аlex
         int count = 0;
         for (int i = 0; i < persons.length; i++) {
-            if (persons[i].getName().equals(name)) {
+            if (persons[i].getName().equalsIgnoreCase(name)) {
                 return count;
             }
             count++;
@@ -40,6 +40,19 @@ public class PersonRegistry {
         for (int i = 0; i < persons.length; i++) {
             int age1 = persons[i].getAge();
             if (age1 >= MIN_AGE_PERSON && age1 <= MAX_AGE_PERSON) {
+                if (persons[i].getSex() == SEX_PERSON) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int fitForServiceMan() { // мужчины, годные к службе в возрасте от 25 до 27 лет
+        int count = 0;
+        for (int i = 0; i < persons.length; i++) {
+            int age1 = persons[i].getAge();
+            if (age1 >= 25 && age1 <= 27) {
                 if (persons[i].getSex() == SEX_PERSON) {
                     count++;
                 }
--- Lesson6/src/main/java/com/home/my_package/MillitaryOffice.java
+++ Lesson6/src/main/java/com/home/my_package/MillitaryOffice.java
@@ -4,80 +4,72 @@ import com.home.model.Address;
 import com.home.model.Person;
 import com.home.model.PersonRegistry;
 
+import java.util.Arrays;
+
+
 public class MillitaryOffice {
 
     public static void main(String[] args) {
 
-        Address OlegAdress = new Address("Belarus","Minsk");
-        Person Oleg = new Person("Oleg", 15,"male", OlegAdress);
+        Address OlegAdress = new Address("Belarus", "Minsk");
+        Person Oleg = new Person("Oleg", 15, "male", OlegAdress);
 
-        Address AlexAdress = new Address("Belarus","Minsk");
-        Person Alex = new Person("Alex", 25,"male", AlexAdress);
+        Address AlexAdress = new Address("Russia", "Moscow");
+        Person Alex = new Person("Alex", 25, "male", AlexAdress);
 
-        Address IgorAdress = new Address("Belarus","Minsk");
-        Person Igor = new Person("Igor", 35,"male", IgorAdress);
+        Address IgorAdress = new Address("Belarus", "Minsk");
+        Person Igor = new Person("Igor", 35, "male", IgorAdress);
 
-        Address VasyaAdress = new Address("Belarus","Minsk");
-        Person Vasya = new Person("Vasya", 22,"male", VasyaAdress);
+        Address VasyaAdress = new Address("Belarus", "Minsk");
+        Person Vasya = new Person("Vasya", 22, "male", VasyaAdress);
 
-        Address InnaAdress = new Address("Belarus","Minsk");
-        Person Inna = new Person("Inna", 48,"female", InnaAdress);
+        Address InnaAdress = new Address("Belarus", "Minsk");
+        Person Inna = new Person("Inna", 48, "female", InnaAdress);
 
-        Address KonstantinAdress = new Address("Belarus","Minsk");
-        Person Konstantin = new Person("Konstantin", 27,"male", KonstantinAdress);
+        Address KonstantinAdress = new Address("Belarus", "Minsk");
+        Person Konstantin = new Person("Konstantin", 27, "male", KonstantinAdress);
 
-        Address ValentinAdress = new Address("Belarus","Minsk");
-        Person Valentin = new Person("Valentin", 35,"male", ValentinAdress);
+        Address ValentinAdress = new Address("Belarus", "Mogilev");
+        Person Valentin = new Person("Valentin", 35, "male", ValentinAdress);
 
-        Address ZakharAdress = new Address("Belarus","Minsk");
-        Person Zakhar = new Person("Zakhar", 18,"male", ZakharAdress);
+        Address ZakharAdress = new Address("Belarus", "Minsk");
+        Person Zakhar = new Person("Zakhar", 18, "male", ZakharAdress);
 
-        Address IvanAdress = new Address("Belarus","Minsk");
-        Person Ivan = new Person("Ivan", 19,"male", IvanAdress);
+        Address IvanAdress = new Address("Belarus", "Minsk");
+        Person Ivan = new Person("Ivan", 19, "male", IvanAdress);
 
-        Address MaksAdress = new Address("Belarus","Minsk");
-        Person Maks = new Person("Maks", 20,"male", MaksAdress);
+        Address MaksAdress = new Address("Belarus", "Minsk");
+        Person Maks = new Person("Maks", 20, "male", MaksAdress);
 
-        Address FedorAdress = new Address("Belarus","Minsk");
-        Person Fedor = new Person("Fedor", 21,"male", FedorAdress);
+        Address FedorAdress = new Address("Belarus", "Minsk");
+        Person Fedor = new Person("Fedor", 21, "male", FedorAdress);
 
-        Address EdwardAdress = new Address("Belarus","Minsk");
-        Person Edward = new Person("Edward", 23,"male", EdwardAdress );
+        Address EdwardAdress = new Address("Belarus", "Minsk");
+        Person Edward = new Person("Edward", 23, "male", EdwardAdress);
 
-        Address SergeyAdress = new Address("Belarus","Minsk");
-        Person Sergey = new Person("Sergey", 28,"male", SergeyAdress);
+        Address SergeyAdress = new Address("Belarus", "Minsk");
+        Person Sergey = new Person("Sergey", 28, "male", SergeyAdress);
 
-        Address PavelAdress = new Address("Belarus","Minsk");
-        Person Pavel = new Person("Pavel", 25,"male", PavelAdress);
+        Address PavelAdress = new Address("Belarus", "Orsha");
+        Person Pavel = new Person("Pavel", 25, "male", PavelAdress);
 
-        Address VictorAdress = new Address("Belarus","Minsk");
-        Person Victor = new Person("Victor", 26,"male", VictorAdress);
+        Address VictorAdress = new Address("Belarus", "Minsk");
+        Person Victor = new Person("Victor", 26, "male", VictorAdress);
 
-
-        Person[] person = {Oleg,Alex,Igor,Vasya,Inna,Konstantin,Valentin,Zakhar,Ivan,Maks, Fedor,Edward,Sergey,
+        Person[] persons = {Oleg, Alex, Igor, Vasya, Inna, Konstantin, Valentin, Zakhar, Ivan, Maks, Fedor, Edward, Sergey,
                 Pavel, Victor};
-        PersonRegistry personRegistry = new PersonRegistry(person);
-        //personRegistry.countPeople(VictorAdress);
-        System.out.println(personRegistry.countPeople(VictorAdress));
 
+        PersonRegistry personRegistry = new PersonRegistry(persons);
 
-      //  System.out.println(person);
-
-
-
-
-
-
-
-      //  Person[] peoples = new Person[];
-      //  new PersonRegistry(peoples);
-
-
-
+        System.out.println("Всего годных к службе: " + personRegistry.fitForService());
+        System.out.println("Всего в проживающих в городе Минске: " + personRegistry.countPeople(new Address("Belarus", "Minsk")));
+        System.out.println("Всего с именем Alex: " + personRegistry.countPeopleAlex("Alex"));
+        System.out.println("Всего мужчин в озрасте от 25 до 27 лет: " + personRegistry.fitForServiceMan());
     }
 
+}
 
-    }
+
 
 
 

07906e3fee652d3d5ead0b93f71a3dfde493e20b
NadezdaOsipova <kaliadina@mail.ru>
2021-08-08 02:29:38 +0300

    Lesson12

---
+++ Lesson12/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import com.hw1.service.*;
+
+public class Main {
+    public static void main(String[] args) {
+        startCar();
+    }
+
+
+}
---
+++ Lesson12/src/main/java/com/hw1/service/CarImpl.java
@@ -0,0 +1,17 @@
+package com.hw1.service;
+
+import com.hw1.service.Car;
+
+import java.util.Scanner;
+
+public class CarImpl implements Car {
+    @Override
+    public void startCar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Введите число: ");
+        int w = scanner.nextInt();
+
+
+    }
+}
+

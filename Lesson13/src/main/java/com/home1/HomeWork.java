96aaf61cec2ff907b0013efa45674a60a455af70
NadezdaOsipova <kaliadina@mail.ru>
2021-08-07 19:31:55 +0300

    Lesson13

--- Lesson13/src/main/java/com/home1/HomeWork.java
+++ Lesson13/src/main/java/com/home1/HomeWork.java
@@ -17,29 +17,29 @@ public class HomeWork {
         System.out.println("----------------------");
 
         //замена символов вхождения
-        char f1 = str3.charAt(3);
-        char f2 = str3.charAt(0);
-        System.out.println(str3);
-        System.out.println(str3.replace(f1, f2));
+        char f1 = str3.charAt(3);//переменная для 3 индекса
+        char f2 = str3.charAt(0);//переменная для 0 индекса
+        System.out.println(str3);//вывожу текст для наглядности
+        System.out.println(str3.replace(f1, f2));//замена индекса 3 на 0 индекс
         System.out.println("----------------------");
 
        // вывести слова палиндромы
-        System.out.println(str1);
-        System.out.println("...");
-        String[] str = str1.split(" ");
-        for (int i = 0; i < str.length; i++) {
-            String reverseString = new StringBuilder(str[i]).reverse().toString();
-            if (reverseString.equalsIgnoreCase(str[i])) {
-                System.out.println(str[i] + " является словом палиндромом");
+        System.out.println(str1);//вывожу текст для наглядности
+        System.out.println("...");//разделяю текст для наглядности
+        String[] str = str1.split(" ");//разделяю строку на слова, записывая в массив
+        for (int i = 0; i < str.length; i++) {//бегу по массиву
+            String reverseString = new StringBuilder(str[i]).reverse().toString();//создается строчка-реверс
+            if (reverseString.equalsIgnoreCase(str[i])) {//сравнивание изначальной строки со строкой-реверсом
+                System.out.println(str[i] + " является словом палиндромом");//вывод на консоль
             }
         }
         System.out.println("----------------------");
 
-        //вывести предложения с 3-5 словами
+        //вывести предложения с 3-5 словами либо словом палиндромом
         System.out.println(str4); //вывожу текст для наглядности
         System.out.println("...");//разделяю текст для наглядности
         String delimeter = "\\.";//ввожу переменную по которой будет текст разделяться на предложения
-        String[] subStr = str4.split(delimeter);//массив предложений, разделееный переменной
+        String[] subStr = str4.split(delimeter);//массив предложений, разделеный переменной
 
         for (int i = 0; i < subStr.length; i++) {//бегу по массиву предложений
             String[] result = subStr[i].split(" ");//разделяю каждое предложение на слова
